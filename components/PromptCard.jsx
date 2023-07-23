918bb69c359b067368783ba23f5fb6e5d9ee2449
effham.ali <athereffham@gmail.com>
2023-07-23 15:05:42 +0500

    DONE ALMOSTTT

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,14 +7,15 @@ import React, { useState } from "react";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("");
-
+  const { data: session } = useSession();
+  const pathName = usePathname();
   const handleCopy = () => {
-    setCopied(post.prompt)
+    setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
-    setTimeout(()=> setCopied(""),3000)
+    setTimeout(() => setCopied(""), 3000);
   };
 
-  return (
+   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
@@ -54,6 +55,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       >
         {post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p
+            className="font-inter text-sm green_gradient cursor-pointer"
+            onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+            className="font-inter text-sm orange_gradient cursor-pointer"
+            onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
